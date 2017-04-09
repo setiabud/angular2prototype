71ecbddc56cb65dad9d6393fb2510317fc05a9db
Johannes Setiabudi <setiabud@hotmail.com>
2017-04-09 01:15:55 -0400

    add test project back into soution

--- tests/unit/Controllers/WeatherControllerTEsts.cs
+++ tests/unit/Controllers/WeatherControllerTEsts.cs
@@ -1,21 +1,21 @@
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using angular2prototype.web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace angular2prototype.web.tests.Controllers
 {
-    [TestClass]
-    public class HomeControllerTests
-    {
-        [TestMethod]
-        public void Index_Should_Return_Correct_View()
-        {
+	[TestClass]
+	public class WeatherControllerTests
+	{
+		[TestMethod]
+		public void Index_Should_Return_Correct_View()
+		{
 			HomeController controller = new HomeController();
 
 			ViewResult result = controller.Index() as ViewResult;
 
 			Assert.IsNull(result.ViewName);
-        }
-    }
+		}
+	}
 }
